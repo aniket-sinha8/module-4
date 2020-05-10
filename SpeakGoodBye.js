e71d9111451919cb31e1ad233f402aa106b551c7
Aniket Kumar Sinha <56578304+aniket-sinha8@users.noreply.github.com>
2020-05-10 19:32:49 +0530

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,13 @@
+(function (window) {
+var byeSpeaker = {};
+var speakWord = "Good Bye";
+byeSpeaker.speak=function(name) {
+  console.log(speakWord + " " + byeSpeaker.name);
+  }
+
+ window.byeSpeaker = byeSpeaker;
+
+})(window);
+
+
+
